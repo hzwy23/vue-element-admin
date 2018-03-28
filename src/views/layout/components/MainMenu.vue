6674bf42794672ad06e1c95dce475e0845b78fc5
hzwy23 <hzwy23@163.com>
2018-03-28 11:45:01 +0800

    update

--- src/views/layout/components/MainMenu.vue
+++ src/views/layout/components/MainMenu.vue
@@ -6,9 +6,17 @@
 
         <!--主菜单展示区域-->
         <div class="main-menu-content">
-
+          <!--TODO 替换成从数据库读取用户被授权的菜单信息-->
+          <ul>
+            <li>人事管理</li>
+            <li>权限管理</li>
+            <li>商品管理</li>
+            <li>供应商管理</li>
+            <li>仓库管理</li>
+            <li>财股管理</li>
+            <li>报表系统</li>
+          </ul>
         </div>
-
         <!--用户信息展示区域-->
         <div class="main-user-info">
           <navbar></navbar>
@@ -41,11 +49,32 @@
     font-size: 26px;
     font-weight: 600;
     color: #FFFFFF;
-    padding-left: 30px;
+    padding-left: 15px;
     float: left;
   }
   .main-menu-content {
-    float: right;
+    float: left;
+    margin-left: 60px;
     /*width: 600px;*/
+  }
+  .main-menu-content ul {
+    line-height: 60px;
+    height: 60px;
+    padding: 0px;
+    margin: 0px;
+  }
+  .main-menu-content li {
+    float: left;
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: 600;
+    margin: 0px 15px;
+    padding: 0px;
+    height: 60px;
+    line-height: 60px;
+    list-style: none;
+  }
+  .main-user-info {
+
   }
 </style>
