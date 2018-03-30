a69ea85af8f0b0bf932e1260e01ed94b9beaab4e
hzwy23 <hzwy23@163.com>
2018-03-30 09:23:34 +0800

    update

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
 	NODE_ENV: '"production"',
 	ENV_CONFIG: '"prod"',
-	BASE_API: '"http://49.4.10.118:8790"'
+	BASE_API: '"http://49.4.10.118:8790"',
+  MOCK_BASE_API: '"http://49.4.10.118:87900"'
 }
