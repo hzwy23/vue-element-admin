0d723b3d4f1ecd517be3b74b62c4f8305724d050
hzwy23 <hzwy23@163.com>
2018-03-30 08:37:54 +0800

    update

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,5 @@
 module.exports = {
 	NODE_ENV: '"development"',
 	ENV_CONFIG: '"dev"',
-	// BASE_API: '"http://localhost:8790"'
-	BASE_API: '"http://49.4.10.118:8790"'
+  BASE_API: '"http://49.4.10.118:8790"'
 }
